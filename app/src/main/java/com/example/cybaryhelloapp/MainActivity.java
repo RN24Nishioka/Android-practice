53b5739ab211c983fd1b0ed85bb9cb026cb7902f
RyotaNISHIOKA <mlf.r24oka@gmail.com>
2020-08-01 15:35:55 +0900

    #15 Image View Tutorial - Android Development Tutorial 2020 in Java https://www.youtube.com/watch?v=HlzW8pwc99A&list=PLL-4P1BOZnWyGo-7VP5YuB4qa94T0M_4v&index=15

--- app/src/main/java/com/example/cybaryhelloapp/MainActivity.java
+++ app/src/main/java/com/example/cybaryhelloapp/MainActivity.java
@@ -2,58 +2,45 @@ package com.example.cybaryhelloapp;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.Spinner;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
-    int sum = 0;
-    int currentIncrement;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Spinner mySpinner = findViewById(R.id.spinner);
-        final TextView textViewSum = findViewById(R.id.txtSum);
-        Button buttonSum = findViewById(R.id.btn_sum);
+        Button buttonHappy = findViewById(R.id.btn_happy);
+        Button buttonNeutral = findViewById(R.id.btn_neutral);
+        Button buttonSad = findViewById(R.id.btn_sad);
+        final ImageView myImageView = findViewById(R.id.imageView);
 
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.increment_array, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-        mySpinner.setAdapter(adapter);
-        mySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String selectedIncrement = parent.getItemAtPosition(position).toString();
-                currentIncrement = Integer.valueOf(selectedIncrement);
-
-                //Toast.makeText(MainActivity.this, selectedIncrement, Toast.LENGTH_SHORT).show();
-
-
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
-
-        buttonSum.setOnClickListener(new View.OnClickListener() {
+        buttonHappy.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                sum = sum + currentIncrement;
-                textViewSum.setText(String.valueOf(sum));
+                myImageView.setImageResource(R.drawable.ic_baseline_sentiment_very_satisfied_96);
             }
         });
 
+        buttonNeutral.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myImageView.setImageResource(R.drawable.ic_baseline_sentiment_satisfied_96);
+
+            }
+        });
+
+        buttonSad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myImageView.setImageResource(R.drawable.ic_baseline_sentiment_very_dissatisfied_96);
+
+            }
+        });
 
     }
 }
