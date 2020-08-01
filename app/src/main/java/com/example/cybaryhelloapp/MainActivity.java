f2a6b35e0c6b90ea23ad66b45a6373d3d92f6a12
RyotaNISHIOKA <mlf.r24oka@gmail.com>
2020-08-01 12:48:11 +0900

    #14 Android Spinner Example - Android Development Tutorial 2020 in Java https://www.youtube.com/watch?v=0f03wRVs0KI&list=PLL-4P1BOZnWyGo-7VP5YuB4qa94T0M_4v&index=14

--- app/src/main/java/com/example/cybaryhelloapp/MainActivity.java
+++ app/src/main/java/com/example/cybaryhelloapp/MainActivity.java
@@ -1,22 +1,20 @@
 package com.example.cybaryhelloapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import static android.app.ProgressDialog.show;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    int count = 0;
+    int sum = 0;
+    int currentIncrement;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,23 +22,35 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Spinner mySpinner = findViewById(R.id.spinner);
+        final TextView textViewSum = findViewById(R.id.txtSum);
+        Button buttonSum = findViewById(R.id.btn_sum);
 
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.languages_array, android.R.layout.simple_spinner_item);
-
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.increment_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         mySpinner.setAdapter(adapter);
         mySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String selectedLanguage = parent.getItemAtPosition(position).toString();
-                Toast.makeText(MainActivity.this, selectedLanguage, Toast.LENGTH_SHORT).show();
+                String selectedIncrement = parent.getItemAtPosition(position).toString();
+                currentIncrement = Integer.valueOf(selectedIncrement);
+
+                //Toast.makeText(MainActivity.this, selectedIncrement, Toast.LENGTH_SHORT).show();
+
 
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
 
+            }
+        });
+
+        buttonSum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sum = sum + currentIncrement;
+                textViewSum.setText(String.valueOf(sum));
             }
         });
 
