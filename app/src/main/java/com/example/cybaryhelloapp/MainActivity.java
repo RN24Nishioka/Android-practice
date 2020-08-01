dcc9d9f67e64d0b9a07606446fbe03777f1d4409
RyotaNISHIOKA <mlf.r24oka@gmail.com>
2020-08-01 17:28:01 +0900

    #16 Android CheckBox Tutorial - Android Development Tutorial 2020 in Java https://www.youtube.com/watch?v=oKVMIv4u7d4&list=PLL-4P1BOZnWyGo-7VP5YuB4qa94T0M_4v&index=16

--- app/src/main/java/com/example/cybaryhelloapp/MainActivity.java
+++ app/src/main/java/com/example/cybaryhelloapp/MainActivity.java
@@ -3,44 +3,55 @@ package com.example.cybaryhelloapp;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.CheckBox;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+//    Button btn_order = findViewById(R.id.button);
+//    final CheckBox cbx_order = findViewById(R.id.cbx_order);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button buttonHappy = findViewById(R.id.btn_happy);
-        Button buttonNeutral = findViewById(R.id.btn_neutral);
-        Button buttonSad = findViewById(R.id.btn_sad);
-        final ImageView myImageView = findViewById(R.id.imageView);
 
-        buttonHappy.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                myImageView.setImageResource(R.drawable.ic_baseline_sentiment_very_satisfied_96);
-            }
-        });
 
-        buttonNeutral.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                myImageView.setImageResource(R.drawable.ic_baseline_sentiment_satisfied_96);
+//        btn_order.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                boolean isSelected = cbx_order.isChecked();
+//                if (isSelected){
+//                    Toast.makeText(MainActivity.this, "I want some Pizza!", Toast.LENGTH_SHORT).show();
+//                }else{
+//                    Toast.makeText(MainActivity.this, "Nah", Toast.LENGTH_SHORT).show();
+//                }
+//
+//
+//            }
+//        });
 
-            }
-        });
+    }
 
-        buttonSad.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                myImageView.setImageResource(R.drawable.ic_baseline_sentiment_very_dissatisfied_96);
 
-            }
-        });
+    public  void onCheckeboxClicked(View view){
+        boolean checked = ((CheckBox)view).isChecked();
 
+    if(view.getId() == R.id.cbx_order){
+        if(checked)
+            Toast.makeText(this, "I wanna eat Pizza", Toast.LENGTH_SHORT).show();
+        else {
+            Toast.makeText(this, "I'm fine", Toast.LENGTH_SHORT).show();
+        }
+    }
+    else if(view.getId() == R.id.cbx_icecream){
+        if(checked)
+            Toast.makeText(this, "I wanna take some cool IceCream", Toast.LENGTH_SHORT).show();
+        else {
+            Toast.makeText(this, "Nah", Toast.LENGTH_SHORT).show();
+        }
+    }
     }
 }
