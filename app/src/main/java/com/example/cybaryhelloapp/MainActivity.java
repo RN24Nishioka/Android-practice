8d5f26c2389e0aaaba9f43656368bf715f511599
RyotaNISHIOKA <mlf.r24oka@gmail.com>
2020-08-04 15:05:35 +0900

    #21 Explicit Intents on Android - Android Development Tutorial 2020 in Java https://www.youtube.com/watch?v=3jn5pmmYfbI&list=PLL-4P1BOZnWyGo-7VP5YuB4qa94T0M_4v&index=21

--- app/src/main/java/com/example/cybaryhelloapp/MainActivity.java
+++ app/src/main/java/com/example/cybaryhelloapp/MainActivity.java
@@ -1,31 +1,31 @@
 package com.example.cybaryhelloapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.DividerItemDecoration;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
-    RecyclerView recyclerView;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
-        recyclerView = findViewById(R.id.recyclerview);
 
-        String[] languages = {"Java", "Python", "C++", "Ruby", "Javascript", "C#", "C", "Kotlin", "Swift", "Objective C", "Ruby", "Scale", "Clojure", "Rust", "PHP", "Go", "Groovy"};
+        Button btn = findViewById(R.id.button);
 
-        LanguageAdapter languageAdapter = new LanguageAdapter(languages);
-        recyclerView.setAdapter(languageAdapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
 
-        RecyclerView.ItemDecoration itemDecoration = new
-                DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
-        recyclerView.addItemDecoration(itemDecoration);
 
     }
 }
