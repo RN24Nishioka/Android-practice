638eb239721f3de0f2255dea5d87349569be1336
RyotaNISHIOKA <mlf.r24oka@gmail.com>
2020-08-05 16:23:28 +0900

    #25 Why background processes - Android Development Tutorial 2020 in Java https://www.youtube.com/watch?v=LKfx3xZUCHY&list=PLL-4P1BOZnWyGo-7VP5YuB4qa94T0M_4v&index=25

--- app/src/main/java/com/example/cybaryhelloapp/MainActivity.java
+++ app/src/main/java/com/example/cybaryhelloapp/MainActivity.java
@@ -11,33 +11,31 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SharedPreferences sharedPreferences = getPreferences(Context.MODE_PRIVATE);//getSharedPreferences("cybraryAppSetting", Context.MODE_PRIVATE);
-
-        String check = sharedPreferences.getString("firstRun", "true");
-
-        if(check.equals("true")) {
-
-            sayWelcome();
-
-            SharedPreferences.Editor editor = sharedPreferences.edit();
-            editor.putString("firstRun", "false");
-            editor.apply();
-        }else {
-            Toast.makeText(this, "Welcome back!", Toast.LENGTH_LONG).show();
-        }
-
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                heavyProcess();
+                Toast.makeText(MainActivity.this, "Hey!!!", Toast.LENGTH_SHORT).show();
+            }
+        });
 
 
     }
 
-    public void sayWelcome(){
-        Toast.makeText(this, "Welcom New user!", Toast.LENGTH_LONG).show();
+    public void heavyProcess(){
+        try{
+            Thread.sleep(3000);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        };
     }
+
 }
