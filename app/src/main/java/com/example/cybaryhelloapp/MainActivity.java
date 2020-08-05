7a5463866c97e8ed5607ebb38c42c75ef28a7070
RyotaNISHIOKA <mlf.r24oka@gmail.com>
2020-08-05 15:31:24 +0900

    #24 Shared Preferences Demo - Android Development Tutorial 2020 in Java https://www.youtube.com/watch?v=YuELdV9MwD0&list=PLL-4P1BOZnWyGo-7VP5YuB4qa94T0M_4v&index=24

--- app/src/main/java/com/example/cybaryhelloapp/MainActivity.java
+++ app/src/main/java/com/example/cybaryhelloapp/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.cybaryhelloapp;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,24 +18,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btn = findViewById(R.id.button);
+        SharedPreferences sharedPreferences = getPreferences(Context.MODE_PRIVATE);//getSharedPreferences("cybraryAppSetting", Context.MODE_PRIVATE);
 
-        final String message = "I love Cybrary";
+        String check = sharedPreferences.getString("firstRun", "true");
 
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        if(check.equals("true")) {
 
-                Intent intent = new Intent();
-                intent.setAction(Intent.ACTION_SEND);
-                intent.putExtra(Intent.EXTRA_TEXT, message);
-                intent.setType("text/plain");
+            sayWelcome();
 
-                startActivity(intent);
-
-            }
-        });
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putString("firstRun", "false");
+            editor.apply();
+        }else {
+            Toast.makeText(this, "Welcome back!", Toast.LENGTH_LONG).show();
+        }
 
 
+
+    }
+
+    public void sayWelcome(){
+        Toast.makeText(this, "Welcom New user!", Toast.LENGTH_LONG).show();
     }
 }
