022a8d929af7df3ef011dd580c1a0a1d40d318ac
RyotaNISHIOKA <mlf.r24oka@gmail.com>
2020-08-06 22:43:40 +0900

    #27 Network Requests on Android - Android Development Tutorial 2020 in Java https://www.youtube.com/watch?v=drt7Y3SHrPU&list=PLL-4P1BOZnWyGo-7VP5YuB4qa94T0M_4v&index=27

--- app/src/main/java/com/example/cybaryhelloapp/MainActivity.java
+++ app/src/main/java/com/example/cybaryhelloapp/MainActivity.java
@@ -1,69 +1,83 @@
 package com.example.cybaryhelloapp;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
 public class MainActivity extends AppCompatActivity {
-    Button btnSend;
-    EditText eTxtInput;
-    TextView txtOutput;
+
+    ImageView imageView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.imageView);
 
-        btnSend = findViewById(R.id.btn_send);
-        eTxtInput = findViewById(R.id.etxt_input);
-        txtOutput = findViewById(R.id.txt_output);
+        URL url = null;
 
-        btnSend.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String message = eTxtInput.getText().toString();
-                new GetOutputInBackground().execute(message);
-                //performOperation(message);
-            }
-        });
+        try {
+            url = new URL("https://i.ibb.co/L0gS5rZ/room-pic.png");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        new getImageAsync().execute(url);
+
+        //Bitmap bitmap = getRemoteImage(url);
 
     }
 
-    public void performOperation(String message) {
-        String name = getName();
-        String output = name + ":" + message;
-        txtOutput.setText(output);
+    public Bitmap getRemoteImage(URL imageURL){
+
+        try {
+            URLConnection urlConnection = imageURL.openConnection();
+            urlConnection.connect();
+
+            BufferedInputStream bufferedInputStream = new BufferedInputStream(urlConnection.getInputStream());
+
+            Bitmap bitmap = BitmapFactory.decodeStream(bufferedInputStream);
+
+            bufferedInputStream.close();
+
+            return bitmap;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 
-    public String getName(){
-      try {
-          Thread.sleep(5000);
-      }catch (InterruptedException e){
-          e.printStackTrace();
-      }
-
-      return "Jhon";
-    }
-
-    private class GetOutputInBackground extends AsyncTask<String, Void, String>{
+    private class getImageAsync extends AsyncTask<URL, Void, Bitmap>{
 
         @Override
-        protected String doInBackground(String... strings) {
-            String message = strings[0];
-            String name = getName();
-            String output = name + ":" + message;
-            return output;
+        protected Bitmap doInBackground(URL... urls) {
+
+            URL url = urls[0];
+            Bitmap bitmap = getRemoteImage(url);
+
+            return bitmap;
         }
 
         @Override
-        protected void onPostExecute(String s) {
-            txtOutput.setText(s);
-
+        protected void onPostExecute(Bitmap bitmap) {
+            imageView.setImageBitmap(bitmap);
         }
     }
 
