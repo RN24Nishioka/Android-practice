c2c33fb3f60dd0fb94384e885a3f9e8bbb2c250f
RyotaNISHIOKA <mlf.r24oka@gmail.com>
2020-08-06 10:59:23 +0900

    #26 Using AsyncTask for background processes - Android Development Tutorial 2020 in Java https://www.youtube.com/watch?v=v85xYjjIV20&list=PLL-4P1BOZnWyGo-7VP5YuB4qa94T0M_4v&index=26

--- app/src/main/java/com/example/cybaryhelloapp/MainActivity.java
+++ app/src/main/java/com/example/cybaryhelloapp/MainActivity.java
@@ -1,41 +1,71 @@
 package com.example.cybaryhelloapp;
 
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    Button button;
+    Button btnSend;
+    EditText eTxtInput;
+    TextView txtOutput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        btnSend = findViewById(R.id.btn_send);
+        eTxtInput = findViewById(R.id.etxt_input);
+        txtOutput = findViewById(R.id.txt_output);
+
+        btnSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                heavyProcess();
-                Toast.makeText(MainActivity.this, "Hey!!", Toast.LENGTH_SHORT).show();
+                String message = eTxtInput.getText().toString();
+                new GetOutputInBackground().execute(message);
+                //performOperation(message);
             }
         });
 
-
     }
 
-    public void heavyProcess(){
-        try{
-            Thread.sleep(3000);
-        }catch (InterruptedException e){
-            e.printStackTrace();
-        };
+    public void performOperation(String message) {
+        String name = getName();
+        String output = name + ":" + message;
+        txtOutput.setText(output);
     }
+
+    public String getName(){
+      try {
+          Thread.sleep(5000);
+      }catch (InterruptedException e){
+          e.printStackTrace();
+      }
+
+      return "Jhon";
+    }
+
+    private class GetOutputInBackground extends AsyncTask<String, Void, String>{
+
+        @Override
+        protected String doInBackground(String... strings) {
+            String message = strings[0];
+            String name = getName();
+            String output = name + ":" + message;
+            return output;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            txtOutput.setText(s);
+
+        }
+    }
+
 
 }
