fc9a195c94ec081d256981ba976904d02bb99580
RyotaNISHIOKA <mlf.r24oka@gmail.com>
2020-08-10 22:53:09 +0900

    #28 Network Requests With Glide Library - Android Development Tutorial 2020 in Java https://www.youtube.com/watch?v=0CaW5ZdxoOg&list=PLL-4P1BOZnWyGo-7VP5YuB4qa94T0M_4v&index=28

--- app/src/main/java/com/example/cybaryhelloapp/MainActivity.java
+++ app/src/main/java/com/example/cybaryhelloapp/MainActivity.java
@@ -1,22 +1,14 @@
 package com.example.cybaryhelloapp;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.io.BufferedInputStream;
-import java.io.IOException;
+import com.bumptech.glide.Glide;
+
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,56 +21,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         imageView = findViewById(R.id.imageView);
 
-        URL url = null;
-
-        try {
-            url = new URL("https://i.ibb.co/L0gS5rZ/room-pic.png");
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-
-        new getImageAsync().execute(url);
-
-        //Bitmap bitmap = getRemoteImage(url);
-
-    }
-
-    public Bitmap getRemoteImage(URL imageURL){
-
-        try {
-            URLConnection urlConnection = imageURL.openConnection();
-            urlConnection.connect();
-
-            BufferedInputStream bufferedInputStream = new BufferedInputStream(urlConnection.getInputStream());
-
-            Bitmap bitmap = BitmapFactory.decodeStream(bufferedInputStream);
-
-            bufferedInputStream.close();
-
-            return bitmap;
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return null;
-    }
-
-    private class getImageAsync extends AsyncTask<URL, Void, Bitmap>{
-
-        @Override
-        protected Bitmap doInBackground(URL... urls) {
-
-            URL url = urls[0];
-            Bitmap bitmap = getRemoteImage(url);
-
-            return bitmap;
-        }
-
-        @Override
-        protected void onPostExecute(Bitmap bitmap) {
-            imageView.setImageBitmap(bitmap);
-        }
+        Glide.with(this).load("https://i.ibb.co/L0gS5rZ/room-pic.png").into(imageView);
     }
 
 
